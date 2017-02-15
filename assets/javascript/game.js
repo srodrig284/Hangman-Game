56b7faa319d8ac3b088eff3c87bad49ea0b332fb
SandraR <srodrig284@gmail.com>
2017-02-15 15:24:31 -0500

    Used JQuery

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -14,8 +14,6 @@ var pokerTerms = {
     gameEnded: false,
     termsList: ['straight', 'flush', 'ace in the hole', 'dealer', 'blinds', 'bad beat', 'big blind', 'small blind', 'donkey', 'heads up',
     'pocket rockets', 'raise', 'the nuts', 'royal flush', 'kicker', 'gutshot', 'under the gun', 'short stack', 'side pot', 'straddle'],
-
-
     //picks term from the list
     selectTerm: function() {
         this.targetWordArray = [];
@@ -32,20 +30,12 @@ var pokerTerms = {
         this.displayArray = this.updateDisplayArray("_");
 
         // set initial values
-        //document.getElementById('wins').innerHTML = this.wins;
         $("#wins").html(this.wins);
-        //document.getElementById('losses').innerHTML = this.losses;
         $("#losses").html(this.losses);
-        //document.getElementById('livesRemaining').innerHTML = this.livesRemaining;
         $("#livesRemaining").html(this.livesRemaining);
-        //document.getElementById('guess-word').innerHTML = this.displayArray.join(" ");
-        //document.getElementById('guess-word').innerHTML = this.displayArray.toString();
-        $("#guess-word").html(this.displayArray.join(" "));
-        //document.getElementById('letters-used').innerHTML = this.lettersUsed.join(" ");
+        $("#guess-word").html(this.displayArray.join(" ").replace(/\^/gi, "&nbsp" ));
         $("#letters-used").html(this.lettersUsed.join(" "));
-        //document.getElementById('start-message').innerHTML = "Press a letter key to start";
         $("#start-message").html("Press a letter key to start");
-        //document.getElementById('message').innerHTML = "";
         $("#message").html("");
     },
 
@@ -57,22 +47,20 @@ var pokerTerms = {
         var displayArray = this.displayArray;
         // if underscore is passed, initialize display word with underscores
         if(letter === "_"){
-            for (var i = 0; i < this.targetWordArray.length; i++) {
-                if(this.targetWordArray[i] === " "){
-                    displayArray[i] = " ";
+            $.each(this.targetWordArray, function(index, value) {
+                if (value !== " ") {
+                    displayArray.push("_");
+                }  else {
+                    displayArray.push("^");
                 }
-                else{
-                    displayArray[i] = "_";
-                }
-            }
+            });
         }
         else {
-            for (var j = 0; j < this.targetWordArray.length; j++) {
-
-                if (this.targetWordArray[j] === letter) {
-                    displayArray[j] = letter;
+            $.each(this.targetWordArray, function(index, value){
+                if (value === letter){
+                    displayArray[index] = letter;
                 }
-            }
+            });
         }
         return displayArray;
 
@@ -91,7 +79,6 @@ var pokerTerms = {
                     userInput = userInput.toUpperCase();
                     if (this.lettersUsed.indexOf(userInput) == -1 ) {
                         // letter was not used. add to list of used letters
-                        //this.lettersUsed += userInput;
                         this.lettersUsed.push(userInput);
 
                         if (this.targetWord.indexOf(userInput) != -1) {
@@ -114,44 +101,38 @@ var pokerTerms = {
 
     refreshRound: function(isCorrect){
         if (isCorrect) {
-            //document.getElementById('guess-word').innerHTML = this.displayArray.join(" ");
-            $("#guess-word").html(this.displayArray.join(" "));
+            $("#guess-word").html(this.displayArray.join(" ").replace(/\^/gi, "&nbsp" ));
         }
         else {
-            //document.getElementById('livesRemaining').innerHTML = this.livesRemaining;
             $("#livesRemaining").html(this.livesRemaining);
         }
 
-        //document.getElementById('letters-used').innerHTML = this.lettersUsed.join(" ");
         $("#letters-used").html(this.lettersUsed.join(" "));
 
-        if (this.targetWord.split("").toString() === this.displayArray.toString()) {
-                this.endRound(true);  // win
+        // check if the word was guessed
+        var a = this.displayArray.indexOf("_");
+        if (a < 0){
+            this.endRound(true);
         }
         else if (this.livesRemaining == 0) {
-                this.endRound(false); // lose
+            this.endRound(false); // lose
         }
     },
+
 
     endRound: function(winlose){
         if(winlose){
             this.wins += 1;
-            //document.getElementById('message').innerHTML = "YOU WIN!";
             $("#message").html("YOU WIN!");
         }
         else{
             this.losses += 1;
-            //document.getElementById('message').innerHTML = "YOU LOSE!";
             $("#message").html("YOU LOSE!");
         }
         this.gameEnded = true;
-        //document.getElementById('start-message').innerHTML = "Press any key to start";
         $("#start-message").html("Press any key to start");
-        //document.getElementById('wins').innerHTML = this.wins;
         $("#WINS").html(this.wins);
-        //document.getElementById('losses').innerHTML = this.losses;
         $("#losses").html(this.losses);
-        //pokerTerms.startRound();
     }
 
 };
@@ -159,146 +140,8 @@ var pokerTerms = {
 // get new word and start game
 pokerTerms.selectTerm();
 
-//var userInput;
-
 document.onkeyup = function (event) {
     console.log(event.key);
-    //document.getElementById('start-message').innerHTML = "";
     $("#start-message").html("");
     pokerTerms.validateInput(event.key, event.keyCode);
-    /*userInput = event.key;
-    if(letters.test(userInput)){
-        console.log(userInput);
-        pokerTerms.validateInput(userInput);
-    }*/
 };
-
-/*
-document.onkeyup = function (event) {
-    var userInput = String.fromCharCode(event.key).toUpperCase();
-};
-*/
-/*function startNewGame(){
-    window.addEventListener("keydown", function(event) {
-        console.log("KEYCODE: " + event.keyCode);
-        if(event.keyCode == 13){
-            //console.log("Enter key pressed");
-            // Pick a random word
-            hangman.newWord = hangman.wordbankArray[Math.floor(Math.random() * hangman.wordbankArray.length)];
-
-            // Set up the answer array
-            for (var i = 0; i < hangman.newWord.length; i++) {
-
-                hangman.answerArray[i] = "_";
-            }
-
-        }
-        else{
-            var guess = String.fromCharCode(event.keyCode).toUpperCase();
-        }
-    });
-
-}*/
-
-
-
-
-/*function allLetter(inputtxt)
- {
- var letters = /^[A-Za-z]+$/;
- if(inputtxt.value.match(letters))
- {
- return true;
- }
- else
- {
- alert("message");
- return false;
- }
- }
-*/
-
-/*
-/ Create an array of words
-
-var words = [
-
-    "straight",
-
-    "flush",
-
-    "royal",
-
-    "fullboat"
-
-
-];
-
-// Pick a random word
-
-var word = words[Math.floor(Math.random() * words.length)];
-
-// Set up the answer array
-
-var answerArray = [];
-
-for (var i = 0; i < word.length; i++) {
-
-    answerArray[i] = "_";
-
-}
-
-var remainingLetters = word.length;
-
-// The game loop
-
-while (remainingLetters > 0) {
-
-    // Show the player their progress
-
-    alert(answerArray.join(" "));
-
-    // Get a guess from the player
-
-    var guess = prompt("Guess a letter, or click Cancel to stop
-
-    playing.");
-
-    if (guess === null) {
-
-        // Exit the game loop
-
-        break;
-
-    } else if (guess.length !== 1) {
-
-        alert("Please enter a single letter.");
-
-    } else {
-
-        // Update the game state with the guess
-
-        for (var j = 0; j < word.length; j++) {
-
-            if (word[j] === guess) {
-
-                answerArray[j] = guess;
-
-                remainingLetters--;
-
-            }
-
-        }
-
-    }
-
-    // The end of the game loop
-
-}
-
-// Show the answer and congratulate the player
-
-alert(answerArray.join(" "));
-
-alert("Good job! The answer was " + word);
-*/
